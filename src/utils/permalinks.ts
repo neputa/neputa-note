bee5e344a5e37d033e1de797c3d6a6b1f8ae2d44
takuma-home-wsl <takuma.sasakubo@outlook.jp>
2024-07-28 14:22:42 +0900

    fix(custom): fixed html escape on share buttons
    added encodeURIComponent method for blog title

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -1,4 +1,18 @@
 /**
+ * 文字列のhtmlエスケープ処理を行う
+ * @param {string} str:string
+ * @returns {string} htmlエスケープ済み文字列
+ */
+export const htmlEscape = (str: string): string => {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+/**
  * 相対パスをタイプ別に整形し返す
  * 先頭・末尾 '/' 有り
  * @param {string} slug - URLパス
